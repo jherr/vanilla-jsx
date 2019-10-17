31eba97118fb875f467b98e331fb8d9b699acf29
Herrington, Jack <Jack.Herrington@nike.com>
2019-10-17 07:33:19 -0700

    First commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+/** @jsx dom */
+
+const dom = (name, props, ...children) => {
+  const elem = document.createElement(name);
+
+  Object.keys(props || {}).forEach(k => {
+    if (k === 'style') {
+      Object.keys(props[k]).forEach(sk => {
+        elem.style[sk] = props[k][sk];
+      });
+    } else {
+      elem[k] = props[k];
+    }
+  });
+
+  (children || []).forEach(child => {
+    if (typeof child === 'string') {
+      elem.appendChild(document.createTextNode(child));
+    } else if(Array.isArray(child)) {
+      child.forEach(c => elem.appendChild(c));
+    } else {
+      elem.appendChild(child);
+    }
+  });
+  return elem;
+};
+
+const arr = ['Hey', 'There', 'Jack'];
+
+const foo = (
+  <div>
+    <div className="foo" style={{ fontSize: 50 }}>Hi there</div>
+    {arr.map(name => (<div>{name}</div>))}
+  </div>
+);
+
+document.getElementById('test').appendChild(foo);
